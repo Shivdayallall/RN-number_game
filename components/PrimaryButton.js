42d2387032922def8d2bdcc06dcd28f66e3198f0
shivl <shivlall0009@gmail.com>
2025-08-14 18:11:35 -0400

    design and set up the UI for the home screen

---
+++ components/PrimaryButton.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+
+const PrimaryButton = (props) => {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+      >
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#4d1430ff',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
